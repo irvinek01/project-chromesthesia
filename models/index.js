f69486ab5d8140d491bdf89ee06f317be28eed60
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-25 14:00:01 -0700

    Added songs

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  connect: require ("../config/connectDb"),
+  connect: require("../config/connectDb"),
   User: require("./User"),
   Track: require("./Track"),
+  Color: require("./Color"),
 };
