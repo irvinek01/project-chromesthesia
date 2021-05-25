02202d5d2a90fd5ebc7bf74b9c54d837439c4067
myles502 <80232038+myles502@users.noreply.github.com>
2021-05-25 12:46:19 -0700

    model for color playlist

---
+++ models/Color.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose
+
+const colorSchema = new Schema({
+  id: {type: String, required:true },
+  color: { type: String, required: true },
+  keyNote: { type: String, required: true },
+  songList: {type:[Number], required:true},
+  hexaDecValue: {type:String,required:true}
+});
+
+const Color = mongoose.model("Color", colorSchema);
+
+module.exports = Color;
