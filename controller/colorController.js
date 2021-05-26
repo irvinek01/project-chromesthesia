9bcccfb6ca2b75b57e7e1d73d2af7f835774e2db
myles502 <80232038+myles502@users.noreply.github.com>
2021-05-26 11:55:50 -0700

    updated and created for controllers, color route is now added. we are going to test at this point

--- controller/colorController.js
+++ controller/colorController.js
@@ -3,33 +3,33 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.Track
+    db.Color
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Track
+    db.Color
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Track
+    db.Color
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Track
-      .findOneAndUpdate({ trackId: req.params.id }, req.body)
+    db.Color
+      .findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Track
-      .findById({ trackId: req.params.id })
+    db.Color
+      .findById({ id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
