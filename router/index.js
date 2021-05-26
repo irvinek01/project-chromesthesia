54048c26e7150b8c7727e7020b8fcbf82677a7ea
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-26 11:53:15 -0700

    Get trackRouter, tested in insomnia

--- router/index.js
+++ router/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const clientRouter = require("./clientRouter");
 const userRouter = require("./userRouter");
+const trackRouter = require("./trackRouter");
 
 router.use("/api/users", userRouter);
+router.use("/api/tracks", trackRouter);
 
 // server react client and static assets (usually on heroku)
 // create-react-app dev server used in development
