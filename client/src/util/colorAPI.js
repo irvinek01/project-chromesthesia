6431e72fd6ff7d558f977f9ffe4c9e5f305c25e8
Arvin John Bautista <arvinyoung01@gmail.com>
2021-06-02 05:56:58 -0700

    Added 'get album cover by artist & album title' query from discogs API

--- client/src/util/colorAPI.js
+++ client/src/util/colorAPI.js
@@ -1,15 +1,25 @@
 import axios from "axios";
+require("dotenv").config();
 
+const { DISCOGSTOKEN } = process.env;
 const colorAPI = {
-  
+  getAlbumCover: function (artist, albumTitle) {
+    return axios.get(
+      "https://api.discogs.com/database/search?artist=" +
+        artist +
+        "&title=" +
+        albumTitle +
+        "&token=" +
+        DISCOGSTOKEN +
+        "&page=1&per_page=1"
+    );
+  },
   getAllColors: function () {
     return axios.get("/api/colors");
   },
-
   getPlaylistByColor: function (color) {
     return axios.get("/api/colors/" + color);
   },
-
 };
 
 export default colorAPI;
