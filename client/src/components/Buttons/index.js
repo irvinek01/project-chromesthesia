58c94cabc447e267194778d362cc9cec22e62586
myles502 <80232038+myles502@users.noreply.github.com>
2021-07-06 14:22:28 -0700

    changed nav and heading

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -7,15 +7,15 @@ function Buttons({ colorsData, handleClick }) {
         return (
           <Card.Title className="mx-auto"  key={res._id}>
             <button
-              variant="contained"
+              variant={res.color}
               className="float-right md-auto btn-styling"
         
               onClick={handleClick}
-              style={{ backgroundColor: res.hexaDecValue }}
+              style={{ backgroundColor: res.hexaDecValue }} 
               value={res.color}
             >
               {res.keyNote}
-            </button>
+            </button> {' '}
           </Card.Title>
         );
       })}
